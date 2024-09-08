564ec1b5ee82e86e6aca1121b8490531e9a3f1bc
kinamura <i.kosuke0801@gmail.com>
2024-09-08 23:51:53 +0900

    ft_error_message.c

--- srcs/utils/ft_error_message.c
+++ srcs/utils/ft_error_message.c
@@ -6,14 +6,14 @@
 /*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 01:03:49 by kinamura          #+#    #+#             */
-/*   Updated: 2024/09/04 01:03:50 by kinamura         ###   ########.fr       */
+/*   Updated: 2024/09/08 23:51:33 by kinamura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_utils.h"
 
-void	ft_error_message(char *message)
+void	ft_error_message(int fd, char *message)
 {
-	ft_putendl_fd(message, 2);
+	ft_putendl_fd(message, fd);
 	exit(EXIT_FAILURE);
 }
