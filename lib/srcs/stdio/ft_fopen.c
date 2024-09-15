8f8a286d27a24c704830706558b95ad580ba8bcf
kinamura <i.kosuke0801@gmail.com>
2024-09-15 22:22:03 +0900

    ft_fopen

--- lib/srcs/stdio/ft_fopen.c
+++ lib/srcs/stdio/ft_fopen.c
@@ -6,27 +6,29 @@
 /*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 21:15:14 by kinamura          #+#    #+#             */
-/*   Updated: 2024/09/14 10:53:09 by kinamura         ###   ########.fr       */
+/*   Updated: 2024/09/15 10:08:51 by kinamura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "ft_printf.h"
 
-int	ft_fopen(char *filename, char *opt)
+int	ft_fopen(char *filename, const char *mode)
 {
 	int	fd;
 
 	fd = 0;
-	if (ft_strncmp(opt, "wr", 2) == 0 && ft_strlen(opt) == 2)
-		fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0755);
-	else if (ft_strncmp(opt, "w", 1) == 0 && ft_strlen(opt) == 1)
-		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0755);
-	else if (ft_strncmp(opt, "r", 1) == 0 && ft_strlen(opt) == 1)
+	if (ft_strncmp(mode, "w", 1) == 0 && ft_strlen(mode) == 1)
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (ft_strncmp(mode, "a", 1) == 0 && ft_strlen(mode) == 1)
+		fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else if (ft_strncmp(mode, "r", 1) == 0 && ft_strlen(mode) == 1)
 		fd = open(filename, O_RDONLY);
 	else
 		ft_error("Invalid file operation mode.\n");
 	if (fd == -1)
+	{
 		perror("Error opening file");
+		exit(EXIT_FAILURE);
+	}
 	return (fd);
 }
