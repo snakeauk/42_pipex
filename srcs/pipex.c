3891343ee84878289a895cc94da3d0cd99d86204
kinamura <i.kosuke0801@gmail.com>
2024-09-18 23:28:16 +0900

    pipex

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 00:40:55 by kinamura          #+#    #+#             */
-/*   Updated: 2024/09/17 07:29:55 by kinamura         ###   ########.fr       */
+/*   Updated: 2024/09/18 21:32:29 by kinamura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,12 @@
 int	main(int argc, char **argv, char **envp)
 {
 	int		fd[2];
-	pid_t	pid;
 
 	if (argc != 5)
 		ft_error("Error: Invalid number of arguments\n");
 	if (pipe(fd) == -1)
 		ft_error("Error: Failed to create pipe\n");
-	pid = fork();
-	if (pid == -1)
-		ft_error("Error: Failed to fork\n");
-	if (pid == 0)
-		child_in(argv, envp, fd);
-	close(fd[1]);
-	if (waitpid(pid, NULL, 0) == -1)
-		ft_error("Error: waitpid failed\n");
-	pid = fork();
-	if (pid == -1)
-		ft_error("Error: Failed to fork\n");
-	if (pid == 0)
-		child_out(argc, argv, envp, fd);
-	close(fd[0]);
-	if (waitpid(pid, NULL, 0) == -1)
-		ft_error("Error: waitpid failed\n");
+	ft_process_input(argv, envp, fd);
+	ft_process_output(argc - 1, argv, envp, fd);
 	return (0);
 }
