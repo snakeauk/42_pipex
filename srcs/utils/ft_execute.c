7e94ecf824b7e88eb556a7d63b2ae50369628e86
kinamura <i.kosuke0801@gmail.com>
2024-09-20 00:18:33 +0900

    del some files

--- srcs/utils/ft_execute.c
+++ srcs/utils/ft_execute.c
@@ -6,32 +6,12 @@
 /*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 20:54:25 by kinamura          #+#    #+#             */
-/*   Updated: 2024/09/15 23:07:40 by kinamura         ###   ########.fr       */
+/*   Updated: 2024/09/20 00:10:30 by kinamura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_utils.h"
 
-// void	ft_execute(char *input, char **envp)
-// {
-// 	char	**cmd;
-// 	char	*path;
-
-// 	cmd = ft_split(input, ' ');
-// 	if (!cmd || !cmd[0])
-// 		ft_error("Error: command parsing failed\n");
-// 	path = ft_get_path(cmd[0], envp);
-// 	if (!path)
-// 	{
-// 		ft_array_free(cmd);
-// 		ft_error("Error: command not found\n");
-// 	}
-// 	if (execve(path, cmd, envp) == -1)
-// 	{
-// 		ft_array_free(cmd);
-// 		ft_error("Error: failed to execute command\n");
-// 	}
-// }
 void	ft_execute(char *input, char **envp)
 {
 	char	**cmd;
@@ -49,11 +29,10 @@ void	ft_execute(char *input, char **envp)
 	{
 		ft_array_free(cmd);
 		ft_error("Error: command not found\n");
-		exit(EXIT_FAILURE);
 	}
 	if (execve(path, cmd, envp) == -1)
 	{
-		perror("execve failed");
+		perror("Error: permission denied");
 		ft_array_free(cmd);
 		free(path);
 		exit(EXIT_FAILURE);
--- srcs/utils/ft_get_path.c
+++ srcs/utils/ft_get_path.c
@@ -6,7 +6,7 @@
 /*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 21:25:46 by kinamura          #+#    #+#             */
-/*   Updated: 2024/09/14 17:33:38 by kinamura         ###   ########.fr       */
+/*   Updated: 2024/09/20 00:06:34 by kinamura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ char	*ft_get_path(char *cmd, char **envp)
 		part_path = ft_strjoin(paths[index], "/");
 		path = ft_strjoin(part_path, cmd);
 		free(part_path);
-		if (access(path, F_OK) == 0)
+		if (ft_access(path) == 0)
 		{
 			ft_array_free(paths);
 			return (path);
