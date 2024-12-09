3944f2bd796296092b68b56eb037ee934479ace8
Kosuke Inamura <kinamura@c2r5s1.42tokyo.jp>
2024-12-09 22:54:34 +0900

    write when finish gnl

--- srcs/utils/here_doc.c
+++ srcs/utils/here_doc.c
@@ -6,69 +6,83 @@
 /*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 21:04:40 by kinamura          #+#    #+#             */
-/*   Updated: 2024/12/04 21:04:45 by kinamura         ###   ########.fr       */
+/*   Updated: 2024/12/09 22:20:48 by kinamura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
-int	is_here_doc(char **argv);
-int	read_line(int fd, char *limit);
-int	here_doc(char *limit, t_pipe *data);
+int		is_here_doc(char **argv);
+char	*read_line(char *limit);
+int		write_fd(int fd, char *limit);
+int		here_doc(char *limit, t_pipe *data);
 
 int	is_here_doc(char **argv)
 {
 	return (ft_strncmp(argv[1], "here_doc", 8) == 0 && ft_strlen(argv[1]) == 8);
 }
 
-int	read_line(int fd, char *limit)
+char	*read_line(char *limit)
 {
 	char	*line;
-	int		status;
+	char	*str;
 
-	status = EXIT_SUCCESS;
+	str = NULL;
 	while (1)
 	{
 		write(STDOUT_FILENO, "> ", 2);
 		if (get_next_line(STDIN_FILENO, &line) <= 0)
 		{
-			status = EXIT_FAILURE;
-			break ;
+			free(str);
+			free(line);
+			return (NULL);
 		}
-		if (ft_strlen(line) - 1 == ft_strlen(limit) && (!ft_strncmp(limit, line,
-					ft_strlen(limit))))
+		if (ft_strlen(line) - 1 == ft_strlen(limit)
+			&& (!ft_strncmp(limit, line, ft_strlen(limit))))
 			break ;
-		if (ft_fputs(line, fd) < 0)
-		{
-			status = EXIT_FAILURE;
-			break ;
-		}
+		ft_stradd(&str, line);
 		free(line);
 	}
 	free(line);
-	return (status);
+	return (str);
+}
+
+int	write_fd(int fd, char *limit)
+{
+	char	*str;
+
+	str = read_line(limit);
+	if (!str)
+		return (EXIT_FAILURE);
+	if (ft_fputs(str, fd) < 0)
+	{
+		perror("Error writing to file");
+		free(str);
+		return (EXIT_FAILURE);
+	}
+	free(str);
+	return (EXIT_SUCCESS);
 }
 
 int	here_doc(char *limit, t_pipe *data)
 {
-	int	fd;
-	int	status;
+	int		fd;
 
 	fd = ft_fopen(HEREDOC_TMP_FILE, "w");
 	if (fd <= 0)
 		return (EXIT_FAILURE);
-	status = read_line(fd, limit);
-	close(fd);
-	if (status == EXIT_FAILURE)
+	if (write_fd(fd, limit) != EXIT_SUCCESS)
 	{
 		unlink(HEREDOC_TMP_FILE);
-		return (status);
+		return (EXIT_FAILURE);
 	}
+	if (ft_close(fd) < 0)
+		return (EXIT_FAILURE);
 	data->infile = ft_fopen(HEREDOC_TMP_FILE, "r");
 	if (data->infile <= 0)
 	{
 		unlink(HEREDOC_TMP_FILE);
-		status = EXIT_FAILURE;
+		return (EXIT_FAILURE);
 	}
-	return (status);
+	return (EXIT_FAILURE);
 }
